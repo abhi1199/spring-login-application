0b6c55a5e727f5145d56a5f80e4e403be993cfeb
abhinav <abhinavknl@gmail.com>
2020-06-14 15:13:32 +0530

    add application controller and started a welcome page review

--- src/main/java/com/example/loginapplication/controller/ApplicationController.java
+++ src/main/java/com/example/loginapplication/controller/ApplicationController.java
@@ -1,5 +1,24 @@
 package com.example.loginapplication.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
 public class ApplicationController {
+	
+	@ResponseBody
+	@RequestMapping("/home")
+	public String Hello() {
+		return "hello";
+	}
+	
+	@RequestMapping("/welcome")
+	public String welcome() {
+		//return "hello";
+		return "welcomepage";
+	}
+	
+	
 
 }
